017b60ddceb5eb6e181471387b83899824557d7f
killdash9 <killdash9>
2016-02-28 19:49:06 -0700

    small improvements

--- site-lisp/variable-pitch-modes.el
+++ site-lisp/variable-pitch-modes.el
@@ -8,8 +8,14 @@
 
 (defun variable-pitch-modes-hook ()
   (when (member major-mode variable-pitch-modes)
-    (variable-pitch-mode)))
+    (variable-pitch-mode 1)))
 
 (add-hook 'after-change-major-mode-hook 'variable-pitch-modes-hook)
+;; go through existing buffers
+(mapc
+ (lambda (buf)
+   (with-current-buffer buf
+     (variable-pitch-modes-hook)))
+ (buffer-list))
 
 (provide 'variable-pitch-modes)
