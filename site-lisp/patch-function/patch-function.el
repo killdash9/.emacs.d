a70a7454377354b61be34c3d6de03bbe6a1edf57
Russell Black <rblack@ancestry.com>
2021-01-01 17:34:41 -0700

    Latest

--- site-lisp/patch-function/patch-function.el
+++ site-lisp/patch-function/patch-function.el
@@ -17,12 +17,12 @@ this:
 
 For each [REGEXP REP] pair, replace all occurrences of REGEXP
 with REP in FUNC-SYMBOLS's source definition.  The patches are
-made only in memory and source files on disk are not changed.  It
-works by loading the source code for the function in a buffer,
+made only in memory and source files on disk are left unchanged.
+It works by loading the source code for the function in a buffer,
 patching the code, evaluating it, then reverting the code.  It
 will then close the function's buffer unless it was already open.
 This function is idempotent.  Calling this function with no
-arguments will restore the function's original definition.
+replacements will restore the function's original definition.
 
 REGEXP is a regular expression string to search for in
 FUNC-SYMBOL's definition.  If any of the REGEXPs are not found,
@@ -59,33 +59,58 @@ And here's a patch with a form replacement.
 
   (unless (functionp func-symbol)
     (user-error "First argument must be a function"))
+  (setq replacements (patch-function-collapse replacements))
   (when (= 1 (% (length replacements) 2))
-    (user-error "Expecting an even number of REPLACEMENTS arguments"))
-  (let ((buffer-list (buffer-list)))
+    (user-error "Found a search without a replacement"))
+  (let ((buffer-list (buffer-list))
+        docs-updated)
     (cl-destructuring-bind (buf . point) (find-function-noselect func-symbol t)
       (with-current-buffer buf ;; temporarily switch to the buffer
         ;; create an empty undo stack so that we can revert changes below
         (let ((buffer-undo-list)
               (start (point))
-              end)
+              end
+              (r replacements))
           (save-mark-and-excursion ;; save point and mark
             (unwind-protect
-                (progn
+                (save-restriction
                   (narrow-to-defun)
                   ;; loop over search-replace pairs
-                  (while replacements
-                    (let ((regexp (car replacements))
-                          (rep (cadr replacements)))
-                      ;; convert rep to string if needed
-                      (unless (stringp rep) (setq rep (pp-to-string rep)))
+                  (while r
+                    (let ((regexp (car r))
+                          (rep (cadr r))
+                          (inhibit-read-only t))
                       ;; search and replace
                       (goto-char (point-min))
                       (re-search-forward regexp)
                       (cl-loop do (replace-match rep)
                                while (re-search-forward regexp nil t))
-                     
-                      (setq replacements (cddr replacements))))
+                      (setq r (cddr r))))
 
+                  (when replacements
+                    ;; note in the documentation that is has been patched
+                    (goto-char (point-min))
+                    (when (and replacements (eq (char-after) ?\())
+                      (forward-char)
+                      (when (looking-at "defun")
+                        (forward-sexp 2)
+                        (when (looking-at "[ \t\r\n\v\f] *(")
+                          (forward-sexp)
+                          (if (not (looking-at "[ \t\r\n\v\f] *\""))
+                              (insert "\"\"")
+                            (forward-sexp))
+                          (backward-char)
+                          (when (eq (char-after) ?\")
+                            (insert "\n\nThis function has been patched by the following code\n\n")
+                            (let ((doc (pp-to-string (pp-to-string
+                                                      (append (list 'patch-function `',func-symbol)
+                                                              (mapcar
+                                                               (lambda (s)
+                                                                 (if (not (stringp s)) `',s s))
+                                                               replacements))))))
+                              (insert (substring doc 1 (- (length doc) 1))))
+                            (setq docs-updated t))))))
+                  
                   (setq end (point-max)) ; mark the end of the function
                   ;; widen and eval-region to pick up any lexical
                   ;; scoping directives at top of buffer
@@ -97,8 +122,52 @@ And here's a patch with a form replacement.
               (primitive-undo (length buffer-undo-list) buffer-undo-list)
               ))
           ;; kill the buffer unless it was already open
-          (unless (member buf buffer-list) (kill-buffer))))))
-  (message "Patched function %s" func-symbol))
+          (unless (member buf buffer-list) (kill-buffer)))))
+    (if replacements
+        (if docs-updated
+            (message "Patched function %s" func-symbol)
+          (message "Patched function %s but unable to update its docs" func-symbol))
+      (message "Unpatched function %s" func-symbol))))
+
+(defun patch-function-collapse (l &optional s)
+  "Convert sequences of non-strings in L into into strings.
+S is the string being built as part of the recursion."
+  (let* ((e (car l))
+         (stringp (stringp e))
+         (tail (cdr l)))
+    (cond 
+     ((null l)
+      (if s (list s)))
+     (s
+      (if stringp (cons s (cons e (patch-function-collapse tail)))
+        (patch-function-collapse tail (concat s " " (pp-to-string e)))))
+     (stringp
+      (cons e (patch-function-collapse tail)))
+     (t
+      (patch-function-collapse tail (pp-to-string e))))))
+
+
+(defun patch-function-test-function ()
+  "A test function."
+  (interactive)
+  (let ((o (append
+            '(1 2 replace-me 4 5)
+            '(6 7 8))))
+    (message "%s" o)
+    o)
+  )
+
+(defun test-patch-function ()
+  "Test the patch function."
+  (interactive)
+  (patch-function 'patch-function-test-function
+                  "replace-me"
+                  "3"
+                  "(6 7 8)"
+                  "'(9 10)\n")
+  (cl-assert
+   (equal '(1 2 3 4 5 6 7 8 9 10) (patch-function-test-function))))
+
 (provide 'patch-function)
 
 ;;; patch-function.el ends here
